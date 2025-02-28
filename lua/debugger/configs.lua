6606f7a9a33706016135c8cd70cddd54ef939403
Redoctd <redocted@gmail.com>
2025-02-28 14:02:50 +0200

    make work config require protected in case of them not existing

--- lua/debugger/configs.lua
+++ lua/debugger/configs.lua
@@ -4,7 +4,12 @@ local M = {}
 
 local function insertWorkProjects(targetTable)
   local defaultDotnetVersion = "8.0"
-  local pathConfigs = require 'work.dapConfigs'
+
+  -- try to load work configs.
+  local ok, pathConfigs = pcall(require, 'work.dapConfigs')
+  if not ok then
+    pathConfigs = {}
+  end
 
   for _, config in ipairs(pathConfigs) do
     local basePath = config.basePath
