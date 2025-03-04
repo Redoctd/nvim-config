17a1ba64c5f61b18ca25a814203d762d3a051ab8
Redoctd <redocted@gmail.com>
2025-03-04 15:55:53 +0200

    fix picker config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -25,30 +25,36 @@ return {
 		rename = { enabled = true },
 		input = { enabled = true },
 		statuscolumn = { enabled = true },
-		explorer = { enabled = false },
+		explorer = { enabled = true },
 
-		bigfile = {
-			enabled = true,
-			size = 15 * 1024 * 1024, -- 15MB
-		},
+		-- bigfile = {
+		-- 	enabled = true,
+		-- 	size = 15 * 1024 * 1024, -- 15MB
+		-- },
 		picker = {
 			enabled = true,
 
-			projects = {
-				dev = { "~/HilmaAll/src", "~/HilmaAll/src/hilma-tendering", "~/HilmaAll/src/Hilma", "~/HilmaAll/src/espd", "~/.config", "~/HilmaAll" },
-				patterns = {
-					".git", "_darcs", ".hg", ".bzr", ".svn", "package.json", "Makefile",
-					".csproj", ".sln", "Cargo.toml", "go.mod", "init.lua", ".gitignore",
-					"build.gradle", "build.gradle.kts", "pom.xml", "LICENSE", ".order" },
-			},
-			buffers = {
-				current = false
+			sources = {
+				projects = {
+					dev = { "~/HilmaAll/src", "~/HilmaAll/src/hilma-tendering", "~/HilmaAll/src/Hilma", "~/HilmaAll/src/espd", "~/.config", "~/HilmaAll" },
+					patterns = {
+						".git", "_darcs", ".hg", ".bzr", ".svn", "package.json", "Makefile",
+						"**.csproj", "**.sln", "Cargo.toml", "go.mod", "init.lua", ".gitignore",
+						"build.gradle", "build.gradle.kts", "pom.xml", "LICENSE", ".order" },
+				},
+
+				grep = {
+					buffers = false,
+				},
+				buffers = {
+					current = false
+				}
 			}
 		},
+
 	},
 
 	keys = {
 		{ "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
-		{ "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Search current word",      mode = { "n", "x" } },
 	}
 }
