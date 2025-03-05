804ec9cc60fc606c49401ef85b5acaf73b9d1a0e
Redoctd <redocted@gmail.com>
2025-03-05 12:36:09 +0200

    dont truncate file paths in snacks picker

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -34,6 +34,15 @@ return {
 		picker = {
 			enabled = true,
 
+			formatters = {
+				file = {
+					filename_first = false, -- display filename before the file path
+					truncate = 900, -- truncate the file path to (roughly) this length
+					filename_only = false, -- only show the filename
+					icon_width = 2, -- width of the icon (in characters)
+					git_status_hl = true, -- use the git status highlight group for the filename
+				},
+			},
 			sources = {
 				projects = {
 					dev = { "~/HilmaAll/src", "~/HilmaAll/src/hilma-tendering", "~/HilmaAll/src/Hilma", "~/HilmaAll/src/espd", "~/.config", "~/HilmaAll" },
