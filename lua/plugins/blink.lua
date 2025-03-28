003041861cc6db5a64314e64145f20edeb37f749
Redoctd <redocted@gmail.com>
2025-03-28 16:06:01 +0200

    add dbee blink completion

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,31 +1,50 @@
 return {
-	'saghen/blink.cmp',
-	dependencies = 'rafamadriz/friendly-snippets',
-	version = '*',
-
-	---@module 'blink.cmp'
-	---@diagnostic disable-next-line: undefined-doc-name
-	---@type blink.cmp.Config
-	opts = {
-		-- 'default' for mappings similar to built-in completion
-		-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-		-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-		-- See the full "keymap" documentation for information on defining your own keymap.
-		keymap = { preset = 'super-tab' },
-
-		appearance = {
-			-- Sets the fallback highlight groups to nvim-cmp's highlight groups
-			use_nvim_cmp_as_default = true,
-			-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- Adjusts spacing to ensure icons are aligned
-			nerd_font_variant = 'mono'
-		},
-
-		-- Default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
-		sources = {
-			default = { 'lsp', 'path', 'buffer', 'snippets' },
-		},
+	{
+		"saghen/blink.compat",
+		version = "*",
+		lazy = true,
+		opts = {},
 	},
-	opts_extend = { "sources.default" }
+
+	{
+		"saghen/blink.cmp",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			{ "MattiasMTS/cmp-dbee", dependencies = { "kndndrj/nvim-dbee" }, ft = "sql", opts = {} },
+		},
+		version = "*",
+
+		---@module 'blink.cmp'
+		---@diagnostic disable-next-line: undefined-doc-name
+		---@type blink.cmp.Config
+		opts = {
+			-- 'default' for mappings similar to built-in completion
+			-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+			-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+			keymap = { preset = "super-tab" },
+
+			appearance = {
+				-- Sets the fallback highlight groups to nvim-cmp's highlight groups
+				use_nvim_cmp_as_default = true,
+				nerd_font_variant = "mono",
+			},
+
+			-- Default list of enabled providers defined so that you can extend it
+			-- elsewhere in your config, without redefining it, due to `opts_extend`
+			sources = {
+				default = { "lsp", "path", "buffer", "snippets" },
+				per_filetype = {
+					sql = { "dbee", "buffer", "lsp" },
+				},
+
+				providers = {
+					dbee = {
+						name = "cmp-dbee",
+						module = "blink.compat.source",
+					},
+				},
+			},
+		},
+		opts_extend = { "sources.default" },
+	},
 }
