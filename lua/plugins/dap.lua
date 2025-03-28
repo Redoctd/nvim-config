ead309ea7a087953cb8414aac8938103ad04d35c
Redoctd <redocted@gmail.com>
2025-03-28 16:06:36 +0200

    fix broken dap config

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -7,8 +7,8 @@ local function macSetup()
 			"Cliffback/netcoredbg-macOS-arm64.nvim",
 			dependencies = { "mfussenegger/nvim-dap" },
 			config = function()
-				require('netcoredbg-macOS-arm64').setup(require('dap'))
-			end
+				require("netcoredbg-macOS-arm64").setup(require("dap"))
+			end,
 		}
 	end
 	return nil
@@ -33,15 +33,15 @@ return {
 							hide = {},
 							start_hidden = false,
 						},
-					}
-				}
+					},
+				},
 			},
 			{ "theHamsta/nvim-dap-virtual-text", opts = {} },
 			{
 				"jay-babu/mason-nvim-dap.nvim",
 				opts = { automatic_installation = true },
 			},
-			macSetup()
+			macSetup(),
 		},
 		event = "VeryLazy",
 		config = function()
@@ -52,21 +52,33 @@ return {
 				widgets.centered_float(widgets.scopes, { border = "rounded" })
 			end
 			local binds = {
-				{ "<F5>",        dap.continue,                                     "DAP continue" },
-				{ "<F10>",       dap.step_over,                                    "DAP Step Over" },
-				{ "<F11>",       dap.step_into,                                    "DAP Step Into" },
-				{ "<F12>",       dap.step_out,                                     "DAP Step Out" },
-				{ "<leader>b",   dap.toggle_breakpoint,                            "DAP Breakpoint" },
-				{ "<leader>de",  function() require("dap.ui.widgets").hover() end, "DAP Eval" },
-				{ "<F7>",        function() vim.cmd('DapViewToggle') end,          "DAP View Toggle" },
-				{ "<leader>dvv", openScopes,                                       "DAP Toggle Var View" },
+				{ "<F5>", dap.continue, "DAP continue" },
+				{ "<F10>", dap.step_over, "DAP Step Over" },
+				{ "<F11>", dap.step_into, "DAP Step Into" },
+				{ "<F12>", dap.step_out, "DAP Step Out" },
+				{ "<leader>b", dap.toggle_breakpoint, "DAP Breakpoint" },
+				{
+					"<leader>de",
+					function()
+						require("dap.ui.widgets").hover()
+					end,
+					"DAP Eval",
+				},
+				{
+					"<F7>",
+					function()
+						vim.cmd("DapViewToggle")
+					end,
+					"DAP View Toggle",
+				},
+				{ "<leader>dvv", openScopes, "DAP Toggle Var View" },
 			}
 
-			local utils = require 'utils'
+			local utils = require("utils")
 			utils.BindKeyTableNormalMode(binds)
 
-			dap.adapters = require('debugger.adapters')
-			dap.configurations = require('debugger.configs')
+			-- dap.adapters = require('debugger.adapters')
+			-- dap.configurations = require('debugger.configs')
 		end,
 	},
 }
