1509e6ed5c6f65a51725ec3b204622664b39b928
Redoctd <redocted@gmail.com>
2025-03-28 16:06:59 +0200

    make hard time harder

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,18 +1,4 @@
 return {
 	'm4xshen/hardtime.nvim',
 	config = true,
-	opts = {
-		disabled_keys = {
-			["<Up>"] = {},
-			["<Down>"] = {},
-			["<Left>"] = {},
-			["<Right>"] = {},
-		},
-		restricted_keys = {
-			["<Up>"] = { "n", "x" },
-			["<Down>"] = { "n", "x" },
-			["<Left>"] = { "n", "x" },
-			["<Right>"] = { "n", "x" },
-		}
-	}
 }
